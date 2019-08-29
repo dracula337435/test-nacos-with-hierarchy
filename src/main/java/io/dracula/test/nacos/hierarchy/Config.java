5aa4005c285b59bae9cc8c2a5fdc1ff7dbac9308
dk <dracula337435@163.com>
2019-08-29 17:32:09 +0800

    改用nacos

--- src/main/java/io/dracula/test/nacos/hierarchy/Config.java
+++ src/main/java/io/dracula/test/nacos/hierarchy/Config.java
@@ -1,13 +1,16 @@
 package io.dracula.test.nacos.hierarchy;
 
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author dk
  */
-@PropertySource("args.properties")
+@EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "localhost:8849"))
+@NacosPropertySource(dataId = "test")
 @ComponentScan
 @Configuration
 public class Config {
--- src/main/java/io/dracula/test/nacos/hierarchy/SomeBean.java
+++ src/main/java/io/dracula/test/nacos/hierarchy/SomeBean.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SomeBean {
 
-    @Value("${a}")
+    @Value("${a:default}")
     private String a;
 
     public String getA() {
